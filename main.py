e182f86b95469d966f3953b9780cb6307c59df0f
prachis05 <141426443+prachis05@users.noreply.github.com>
2024-06-19 22:04:27 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
-from menu import Menu, MenuItem
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+from turtle import Turtle,Screen
+import random
 
-money_machine = MoneyMachine()
-coffee_maker = CoffeeMaker()
-menu = Menu()
+is_race_on = False
 
-# coffee_maker.report()
-# money_machine.report()
+screen = Screen()
 
-is_on = True
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make you bet", prompt="Which turtle win the race? Enter the color")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-while is_on:
-    options = menu.get_items()
-    choice = input("What would you like to drink? (espresso/latte/cappuccino)")
-    if choice == "off":
-        is_on = False
-    elif choice == "report":
-        coffee_maker.report()
-        money_machine.report()
-    else:
-        drink = menu.find_drink(choice)
-        if coffee_maker.is_resource_sufficient(drink):
-            if money_machine.make_payment(drink.cost):
-                coffee_maker.make_coffee(drink)
 
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+
+
+if user_bet:
+    is_race_on = True
+
+    while is_race_on:
+        for turtle in all_turtles:
+            if turtle.xcor() > 230:
+                is_race_on = False
+                winning_turtle = turtle.pencolor()
+                if winning_turtle == user_bet:
+                    print(f"You've  won! The {winning_turtle} turtle is the winner ")
+                else:
+                    print(f"You've lost ! The {winning_turtle} turtle is the winner")
+
+            random_no = random.randint(0, 10)
+            turtle.forward(random_no)
+
+
+screen.exitonclick()
