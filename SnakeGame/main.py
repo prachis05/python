c76dd683110924bee62762502dbb34dcd381138d
prachis05 <141426443+prachis05@users.noreply.github.com>
2024-06-19 22:08:50 +0530

    Add files via upload

---
+++ SnakeGame/main.py
@@ -0,0 +1,49 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from score import Scoreboard
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.right, key="Right")
+screen.onkey(fun=snake.left, key="Left")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+        # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        score.reset()
+        snake.reset()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            score.reset()
+            snake.reset()
+
+
+screen.exitonclick()
+
+# Slicing in oython
+# operating on only certain elements of list by slicing them
+# list[2:5]
+# only the elements from 2 to 5 will be displayed
